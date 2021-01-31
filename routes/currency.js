6eb1b7df8151373b413f19d9323f7a2fd172eb6a
Igor Kokorin <kokorin.fear333777@yandex.ru>
2021-02-01 01:30:16 +0700

    api method for getting currency rates

--- routes/currency.js
+++ routes/currency.js
@@ -1,8 +1,23 @@
 const { Router } = require('express');
+const CurrencyFreaks = require('../lib/currency_freaks');
 
 const routes = Router();
 
-routes.get('/rates/:date', (req, res) => res.json({ success: true, date: req.params.date }));
+routes.get('/rates/:date', async (req, res, next) => {
+  try {
+    const cf = new CurrencyFreaks()
+    
+    const rates = await cf.getRatesForDate(req.params.date);
+  
+    return res.json({
+      success: true,
+      rates
+    });
+  } catch (err) {
+    return next(err, req, res);
+  }
+});
+
 routes.get('/compare/:base/:secondary', (req, res) => res.json({ success: true, base: req.params.base, secondary: req.params.secondary }));
 
 module.exports = routes;
