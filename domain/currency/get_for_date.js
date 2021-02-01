446c172f808382bb9fdfd5c99fa4caa5618fe2ad
Igor Kokorin <kokorin.fear333777@yandex.ru>
2021-02-01 13:40:10 +0700

    load rates from currency freakes for today, when there are no

--- domain/currency/get_for_date.js
+++ domain/currency/get_for_date.js
@@ -1,9 +1,37 @@
 const { currency } = require('../../db/models');
+const moment = require('moment');
+const CurrecyFreaks = require('../../lib/currency_freaks');
 
 class GetForDate {
   static async execute (date) {
     if (!date) {
       throw new Error('date is required');
+    }
+
+    if (moment().format('YYYY-MM-DD') === date) {
+      const todayCurrencyRate = await currency.findOne({
+        where: {
+          date
+        }
+      });
+
+      if (!todayCurrencyRate) {
+        const cf = new CurrecyFreaks();
+    
+        const rates = await cf.getCurrentRates();
+
+        await currency.bulkCreate(
+          Object.entries(rates).reduce((acc, curr) => {
+            acc.push({
+              name: curr[0],
+              rate: curr[1],
+              date
+            });
+
+            return acc;
+          }, [])
+        );
+      }
     }
 
     const rates = await currency.findAll({
